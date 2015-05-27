072158003a01e54da22f0b5a7fe660bc12303afe
Ilia Udalov <UdalovIlia@gmail.com>
2015-05-27 23:30:07 +0300

    Refactor

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # merge-images
-Unix like command line utility. It draws images traits on other other image.
+Unix like command line utility. Applies the outline of one image to another.
+
+### Target platform
+Linux
+
+### Build dependencies
+1. OpenCV
+2. pkg-config
 
 ### Build
-1. `cd src && make` 
+1. `cd src && make`
 
 ### Usage
 `./merge <source image> <image traits> [ -o <output>] [-s transparency] [-h]`
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,11 @@
 #include <stdio.h>
-#include <math.h>
-#include "opencv2/core/core.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
-#include "opencv2/photo/photo.hpp"
-#include "opencv2/stitching/stitcher.hpp"
 
-#define DFUNCTION //printf("%s()\n", __FUNCTION__);
-
-#define error(args ...) fprintf(stderr, args); \
-    exit(0);
+#define error(args ...) {       \
+        fprintf(stderr, args);  \
+        exit(0);                \
+    }
 
 const int THRESHOLD1 = 100;
 const int THRESHOLD2 = 240;
@@ -32,15 +28,12 @@ void imshow_resized(const cv::string& winname, const cv::Mat& img)
 }
 
 void print_help(char* name) {
-    printf("Usage: ./%s <source image> <image traits> [ -o <output>] [-s transparency] [-h]\n", name);
+    printf("Usage: %s <source image> <image traits>"
+        " [ -o <output>] [-s opacity] [-h]\n", name);
 }
 
-void print_mat_info(const cv::Mat i, const char* name) {
-    printf("%s\n", name);
-    printf("Cols: %d,\nRows %d\n", i.cols, i.rows);
-}
-
-void check_and_get_args(int argc, char* argv[], std::string& src_img, std::string& mask_img, std::string& out_img, float& how_strong) {
+void check_and_parse_args(int argc, char* argv[], std::string& src_img,
+        std::string& mask_img, std::string& out_img, float& opacity) {
     if (argc < 3) {
         print_help(argv[0]);
         exit(0);
@@ -54,9 +47,7 @@ void check_and_get_args(int argc, char* argv[], std::string& src_img, std::strin
 
     src_img = argv[1];
     mask_img = argv[2];
-    how_strong = 0.3;
-    out_img = "result.jpg";
-
+    opacity = 0.3;
 
     for(int i = 3; i < argc; i++) {
         if (std::string(argv[i]) == "-h") {
@@ -79,18 +70,17 @@ void check_and_get_args(int argc, char* argv[], std::string& src_img, std::strin
             if (transp > 100 || transp < 0) {
                 error("Please enter valid transparency (from 0 to 100)\n");
             }
-            how_strong = static_cast<float>(transp) / 100.0;
+            opacity = static_cast<float>(transp) / 100.0;
 
             continue;
         } else {
             error("No such option: %s\n", argv[i]);
         }
     }
-
 }
 
-cv::Mat transform_mask(const cv::Mat& mask) {
-    DFUNCTION;
+// specify degree of transparency according to the distance to the boundaries
+cv::Mat highlight_contours_on_mask(const cv::Mat& mask) {
     cv::Mat gray(mask);
     cv::cvtColor(mask, gray, CV_BGR2GRAY);
 
@@ -99,33 +89,38 @@ cv::Mat transform_mask(const cv::Mat& mask) {
 
     cv::Mat canny_inv(canny);
     cv::bitwise_not (canny, canny_inv);
-//    imshow_resized("im Canny inverted", canny_inv); // IF NEEDED
 
-    cv::Mat dist(gray);
-    cv::distanceTransform(canny_inv, dist, CV_DIST_L2, 3);
+    // calculate distance to nearest border
+    cv::Mat result(gray);
+    cv::distanceTransform(canny_inv, result, CV_DIST_L2, 3);
 
-    return dist;
+    return result;
 }
 
-cv::Mat merge(const cv::Mat& src, const cv::Mat& mask, const cv::Mat& dist_mask, float how_strong) {
-    DFUNCTION;
+// apply traits of image mask to src image (proportionally dist_mask)
+cv::Mat merge(const cv::Mat& src, const cv::Mat& mask,
+        const cv::Mat& dist_mask, float opacity) {
     cv::Mat res(src);
 
     cv::Mat fit_mask, fit_dist_mask;
     cv::resize(mask, fit_mask, cv::Size(src.cols, src.rows), 1, 1);
     cv::resize(dist_mask, fit_dist_mask, cv::Size(src.cols, src.rows), 1, 1);
 
+    // scale distances
     double min;
     double max;
     cv::minMaxIdx(dist_mask, &min, &max);
 
+    // merge
     for(int chanel = 0; chanel < 3; chanel++) {
         for(int i = 0; i < src.cols; i++) {
             for(int j = 0; j < src.rows; j++) {
                 float distance = fit_dist_mask.at<float>(j, i);
                 float coeff = pow((max - distance) / max, 3);
-                coeff *= how_strong;
-                res.at<cv::Vec3b>(j, i)[chanel] = (1 - coeff) * src.at<cv::Vec3b>(j, i)[chanel]
+                coeff *= opacity;
+
+                res.at<cv::Vec3b>(j, i)[chanel] =
+                    (1 - coeff) * src.at<cv::Vec3b>(j, i)[chanel]
                     + fit_mask.at<cv::Vec3b>(j, i)[chanel] * coeff;
             }
         }
@@ -135,22 +130,22 @@ cv::Mat merge(const cv::Mat& src, const cv::Mat& mask, const cv::Mat& dist_mask,
 
 int main(int argc, char* argv[]) {
     std::string src_img, mask_img, out_img;
-    float how_strong;
+    float opacity;
 
-    check_and_get_args(argc, argv, src_img, mask_img, out_img, how_strong);
+    check_and_parse_args(argc, argv, src_img, mask_img, out_img, opacity);
 
     cv::Mat src = cv::imread(src_img.c_str());
     cv::Mat mask = cv::imread(mask_img.c_str());
 
-    cv::Mat cont_mask = transform_mask(mask);
-    cv::Mat after_merge = merge(src, mask, cont_mask, how_strong);
+    cv::Mat cont_mask = highlight_contours_on_mask(mask);
+    cv::Mat after_merge = merge(src, mask, cont_mask, opacity);
 
-    if (out_img == std::string("")) {
+    if (out_img.empty()) {
         imshow_resized("Result", after_merge);
         cv::waitKey();
     } else {
-        printf("Writing file %s\n", out_img.c_str());
         cv::imwrite( out_img.c_str(), after_merge);
+        printf("Saved to %s\n", out_img.c_str());
     }
     
     return 0;
